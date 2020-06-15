81fc1b98373a60df529fd62ceff831fc3e180dd8
Joshua Serrano <josh@Joshuas-MacBook-Pro.local>
2020-06-14 19:08:10 -0700

    fixes

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import discord
 token_file = open('TOKEN.key', 'r')
 TOKEN = token_file.read().strip()
 token_file.close()
-client=discord.Client()
+client=discord.Client(status="TINK TINK TINK")
 
 @client.event
 async def on_ready():
     print("The bot is ready!")
-    await client.change_presence(game=discord.Game(name="TINK TINK TINK"))
-
+    game = discord.Game("TINK TINK TINK")
+    await client.change_presence(status=discord.Status.idle, activity=game)
 @client.event
 async def on_message(message):
     if "you people" in message.content:
